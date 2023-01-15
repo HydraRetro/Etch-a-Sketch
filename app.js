8684c45d78c3dc787118d9468e095a8183347e5e
HydraRetro <119288194+HydraRetro@users.noreply.github.com>
2023-01-14 17:37:16 -0800

    Refactored Code via Rob

--- app.js
+++ app.js
@@ -1,110 +1,51 @@
-//ETCH-A_SKETCH
+//Etch-A-Sketch
 
-//Grid Sizes
-const grid16 = document.getElementById('16');
-const grid32 = document.getElementById('32');
-const grid64 = document.getElementById('64');
+let color = "black";
+let click = true;
 
+function populateBoard(size) {
+    let container = document.querySelector("#container");
+    let squares = container.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-//Default Grid of 16x16
-function defaultGrid() {
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
-            const node = document.createElement("div");
-            node.classList.add('gridCell');
-            container.appendChild(node);
+    let amount = size * size
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement("div");
+        square.addEventListener("mouseover", colorSquare);
+        square.style.backgroundColor = "white";
+        container.insertAdjacentElement("beforeend", square);
+    }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+        document.querySelector(".error").style.display = "none";
+        populateBoard(input);
+    } else {
+        document.querySelector(".error").style.cssText = "display: flex; justify-content: center; padding: 10px;"
+    }
+}
+
+function colorSquare() {
+    if (click) {
+        if (color === "random") {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = color;
         }
     }
-};
-defaultGrid();
+}
 
-//Change div background color on mouse hover-----------------------------------
-const gridItems = document.querySelectorAll('.gridCell');
-gridItems.forEach((item) => {
-    const gridItem = item;
-    gridItem.count = 0;
-    gridItem.addEventListener('mouseenter', (e) => {
-        e.target.style.backgroundColor = 'black';
-        e.target.style.opacity = 1;
-    })
-});
+function changeColor(choice) {
+    color = choice;
+}
 
-
-//FUTURE ME - FIGURE OUT HOW TO REFACTOR THIS TO BE MUCH MORE CONDENSED
-
-//Adds 16x16 grid in DOM HTML and Styles update.
-
-grid16.addEventListener('click', function () {
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
-            const node = document.createElement("div");
-            node.classList.add('gridCell');
-            container.appendChild(node);
-            //Changes grid to 32x32 with DOM
-            document.getElementById('container').style.gridTemplateColumns = "repeat(16, calc((100% - 16px) / 16))";
-            document.getElementById('container').style.gridTemplateRows = "repeat(16, calc((100% - 16px) / 16))";
-        }
-    };
-    const gridItems = document.querySelectorAll('.gridCell');
-    gridItems.forEach((item) => {
-        const gridItem = item;
-        gridItem.count = 0;
-        gridItem.addEventListener('mouseenter', (e) => {
-            e.target.style.backgroundColor = 'black';
-            e.target.style.opacity = 1;
-        })
-    });
-});
-
-//Adds 32x32 grid in DOM HTML and Styles update.
-grid32.addEventListener('click', function () {
-    for (let i = 0; i < 32; i++) {
-        for (let j = 0; j < 32; j++) {
-            const node = document.createElement("div");
-            node.classList.add('gridCell');
-            container.appendChild(node);
-            //Changes grid to 32x32 with DOM
-            document.getElementById('container').style.gridTemplateColumns = "repeat(32, calc((100% - 32px) / 32))";
-            document.getElementById('container').style.gridTemplateRows = "repeat(32, calc((100% - 32px) / 32))";
-        }
-    };
-    const gridItems = document.querySelectorAll('.gridCell');
-    gridItems.forEach((item) => {
-        const gridItem = item;
-        gridItem.count = 0;
-        gridItem.addEventListener('mouseenter', (e) => {
-            e.target.style.backgroundColor = 'black';
-            e.target.style.opacity = 1;
-        })
-    });
-});
-
-//Adds 64x64 grid in DOM HTML and Styles update
-grid64.addEventListener('click', function () {
-    for (let i = 0; i < 64; i++) {
-        for (let j = 0; j < 64; j++) {
-            const node = document.createElement("div");
-            node.classList.add('gridCell');
-            container.appendChild(node);
-            //Changes grid to 64x64 with DOM
-            document.getElementById('container').style.gridTemplateColumns = "repeat(64, calc((100% - 64px) / 64))";
-            document.getElementById('container').style.gridTemplateRows = "repeat(64, calc((100% - 64px) / 64))";
-        }
-    };
-    const gridItems = document.querySelectorAll('.gridCell');
-    gridItems.forEach((item) => {
-        const gridItem = item;
-        gridItem.count = 0;
-        gridItem.addEventListener('mouseenter', (e) => {
-            e.target.style.backgroundColor = 'black';
-            e.target.style.opacity = 1;
-        })
-    });
-});
-
-
-//Primitive Reset Button
-const resetButton = document.getElementById('buttonReset');
-resetButton.addEventListener('click', function () {
-    location.reload();
-});
+function resetBoard() {
+    let container = document.querySelector("#container");
+    let squares = container.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = "white");
+}
